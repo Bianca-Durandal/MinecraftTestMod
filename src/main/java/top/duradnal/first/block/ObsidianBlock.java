ac992b85e860e3e17fbc43c98658e46029340957
benpaodexiaojuhua <442601340@qq.com>
2021-03-27 21:24:51 +0800

    1.4.1

--- src/main/java/top/duradnal/first/block/ObsidianBlock.java
+++ src/main/java/top/duradnal/first/block/ObsidianBlock.java
@@ -7,7 +7,7 @@ import net.minecraft.block.material.PushReaction;
 
 public class ObsidianBlock extends Block {
 
-    private static Material mine = new Material(MaterialColor.BLUE, false, true, true, true, true, false, false, PushReaction.NORMAL);
+    private final static Material mine = new Material(MaterialColor.BLUE, false, true, true, true, true, false, false, PushReaction.NORMAL);
 
     public ObsidianBlock() {
         super(Properties.create(mine).hardnessAndResistance(5));
--- src/main/java/top/duradnal/first/food/CopperIngot.java
+++ src/main/java/top/duradnal/first/food/CopperIngot.java
@@ -7,9 +7,9 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
 public class CopperIngot extends Item {
-    private static EffectInstance effectInstance = new EffectInstance(Effects.POISON, 3 * 20, 1);
+    private final static EffectInstance effectInstance = new EffectInstance(Effects.POISON, 3 * 20, 1);
 
-    private static Food food = (new Food.Builder())
+    private final static Food food = (new Food.Builder())
             .saturation(10)
             .hunger(20)
             .effect(()->effectInstance,1)
